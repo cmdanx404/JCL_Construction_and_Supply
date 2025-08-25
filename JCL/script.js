a0a44d12b0150c6f4099b824fc8928fc814d6c83
danielmadjus <danielmadjus@gmail.com>
2025-08-25 15:42:33 +0800

    Add files via upload

---
+++ JCL/script.js
@@ -0,0 +1,8 @@
+// Open and Close Form
+document.getElementById("openForm").onclick = function() {
+  document.getElementById("contactForm").style.display = "block";
+};
+
+document.getElementById("closeForm").onclick = function() {
+  document.getElementById("contactForm").style.display = "none";
+};
